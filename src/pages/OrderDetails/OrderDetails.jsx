c1fee7fca1db7f40e3ff8ee6bf536927049f12bc
AKSAR <akshatjaiswal2518@gmail.com>
2025-01-02 21:37:39 +0530

    minor changes

--- src/pages/OrderDetails/OrderDetails.jsx
+++ src/pages/OrderDetails/OrderDetails.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import OrderDetailsCard from "../../components/OrderDetailsCard/OrderDetailsCard";
+import "./OrderDetails.css";
 
 const OrderDetails = () => {
   const { orderId } = useParams();

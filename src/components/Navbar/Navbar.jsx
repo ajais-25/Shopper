42b3d0d15dd030cbba8517321c92ebff44363a5f
Akshat <akshatjaiswal2518@gmail.com>
2025-09-01 23:46:30 +0530

    updated the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,7 @@
-import Logo from "../../images/logo.png";
-import Cart from "../../images/cart.png";
-import Package from "../../images/package.png";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { FiShoppingCart, FiPackage } from "react-icons/fi";
 
 const Navbar = () => {
   const items = useSelector((state) => state.cart.items);
@@ -12,14 +10,14 @@ const Navbar = () => {
     <>
       <div className="navbar">
         <Link to="/" className="logo-container">
-          <img src={Logo} alt="logo" />
+          <span className="logo-text">Shopper</span>
         </Link>
         <div className="navbar-right">
           <Link to="/orders" className="orders-logo-container">
-            <img src={Package} alt="orders" className="orders-logo" />
+            <FiPackage className="orders-icon" />
           </Link>
           <Link to="/cart" className="cart-logo-container">
-            <img src={Cart} alt="cart" className="cart-logo" />
+            <FiShoppingCart className="cart-icon" />
             <div className="cart-quantity">{items.length || 0}</div>
           </Link>
         </div>
